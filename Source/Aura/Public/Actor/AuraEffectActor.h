18dddf2cf4a7063036593550fad395aa7be3c1fa
Nick Reardon <32754140+NickReardon@users.noreply.github.com>
2025-06-30 22:44:44 -0700

    Effect Actor
    Introduces the AAuraEffectActor class with overlap logic to increase health and destroy itself. Adds BP_HealthPotion blueprints and updates StartupMap to include the new actor.
    
    TODO: Uses const_cast to hack a way to modify health, will be replaced once we cover gameplay effects.

---
+++ Source/Aura/Public/Actor/AuraEffectActor.h
@@ -0,0 +1,35 @@
+// Copyright Nicholas Reardon
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/SphereComponent.h"
+#include "AuraEffectActor.generated.h"
+
+UCLASS()
+class AURA_API AAuraEffectActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AAuraEffectActor();
+
+	UFUNCTION()
+	virtual void OnOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	virtual void OnEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	//virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USphereComponent> Sphere;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UStaticMeshComponent> Mesh;
+};
