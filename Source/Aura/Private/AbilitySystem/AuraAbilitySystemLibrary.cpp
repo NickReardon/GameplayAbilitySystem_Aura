6383c1475ad4b69b7e54aaa2762d17b3eae5c019
Nick Reardon <32754140+NickReardon@users.noreply.github.com>
2025-08-10 01:13:44 -0700

    Aura Ability System Blueprint Library
    feat(ui): create blueprint function library for widget controller access
    
    - Implement UAuraAbilitySystemLibrary inheriting from UBlueprintFunctionLibrary
    - Add GetOverlayWidgetController() static function with UFUNCTION(BlueprintPure)
    - Function accepts WorldContextObject parameter for world context tracing
    - Implement chain: WorldContextObject -> PlayerController -> AuraHUD -> OverlayWidgetController
    - Construct FWidgetControllerParameters with PC, PlayerState, ASC, AttributeSet
    - Include proper headers: GameplayStatics, AuraHUD, AuraWidgetController
    - Category: "Aura|Ability System Library|Widget Controller" for blueprint organization
    
    This enables widgets to easily access the overlay widget controller via blueprint nodes,
    eliminating complex class traversal. Function follows established engine patterns for
    static blueprint function libraries with world context object requirements.
    Next: Implement GetAttributeMenuWidgetController() following same pattern.

---
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemLibrary.cpp
@@ -0,0 +1,27 @@
+// Copyright Nicholas Reardon
+
+
+#include "AbilitySystem/AuraAbilitySystemLibrary.h"
+
+#include "Kismet/GameplayStatics.h"
+#include "Player/AuraPlayerState.h"
+#include "UI/HUD/AuraHUD.h"
+
+UOverlayWidgetController* UAuraAbilitySystemLibrary::GetOverlayWidgetController(const UObject* WorldContextObject)
+{
+	if (APlayerController* PC = UGameplayStatics::GetPlayerController(WorldContextObject, 0))
+	{
+		if (AAuraHUD* AuraHUD = Cast<AAuraHUD>(PC->GetHUD()))
+		{
+			AAuraPlayerState* PS = PC->GetPlayerState<AAuraPlayerState>();
+			UAbilitySystemComponent* ASC = PS->GetAbilitySystemComponent();
+			UAttributeSet* AttributeSet = PS->GetAttributeSet();
+			if (AuraHUD && ASC && AttributeSet)
+			{
+				const FWidgetControllerParameters Parameters(PC, PS, ASC, AttributeSet);
+				return AuraHUD->GetOverlayWidgetController(Parameters);
+			}
+		}
+	}
+	return nullptr;
+}
